cbf9fb8d4cbe92264e3c286a7f800e8761c01747
Alex Shaposhnikov <alexxforcode@gmail.com>
2020-09-06 10:23:19 +0300

    version 1.1

---
+++ src/model.js
@@ -0,0 +1,17 @@
+export const model = [
+    {type: 'title', value: 'Test Title', options: {
+        tag: 'h2',
+        styles: 'background: darkred; color: #fff;'
+    }},
+    {type: 'text', value: 'This is  another Test Text', options: {
+        styles: 'background: darkblue; color: yellow;'
+    }},
+    {type: 'textColumns', value: [
+        '1 text',
+        '2 text',
+        '3 text',
+        '4 text'
+    ], options: {
+        styles: 'padding: 1rem;'
+    }}
+]
